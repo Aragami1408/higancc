5ef46bf6f460764c56f144f34c4ae215a43ba49e
Aragami1408 <vucaominh1408@gmail.com>
2024-08-16 22:47:49 +1000

    Use X macros for TokenType enum

--- src/token.c
+++ src/token.c
@@ -1,8 +1,17 @@
 #include "token.h"
 
+
 #include <stdio.h>
+
+#define X(name) [name] = #name ,
+
+const char *token_type_strings[] = {
+  LIST_OF_TOKEN_TYPES
+};
+
+#undef X
 
 void print_token(const Token *token) {
   printf("%4d ", token->line); 
-  printf("%2d '%.*s'\n", token->type, token->length, token->start);
+  printf("%s '%.*s'\n", token_type_strings[token->type], token->length, token->start);
 }
