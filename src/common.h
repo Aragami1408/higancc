cc725525233fb7b275dafba5832c1767b41d9ac4
Aragami1408 <vucaominh1408@gmail.com>
2024-09-25 23:31:44 +1000

    Deleted unnecessary platforms detection

--- src/common.h
+++ src/common.h
@@ -19,44 +19,18 @@ typedef uint64_t u64;
 
 typedef size_t usize;
 
-typedef float f32;
-typedef double f64;
-
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
 #define HIGANCC_PLATFORM_WINDOWS 1
-#ifndef _WIN64
-#error "64-bit is required on Windows!"
-#endif
 #elif defined(__linux__) || defined(__gnu_linux__)
-// Linux OS
 #define HIGANCC_PLATFORM_LINUX 1
-#if defined(__ANDROID__)
-#define HIGANCC_PLATFORM_ANDROID 1
-#endif
 #elif defined(__unix__)
-// Catch anything not caught by the above.
 #define HIGANCC_PLATFORM_UNIX 1
 #elif defined(_POSIX_VERSION)
-// Posix
 #define HIGANCC_PLATFORM_POSIX 1
 #elif __APPLE__
-// Apple platforms
 #define HIGANCC_PLATFORM_APPLE 1
 #include <TargetConditionals.h>
-#if TARGET_IPHONE_SIMULATOR
-// iOS Simulator
-#define HIGANCC_PLATFORM_IOS 1
-#define HIGANCC_PLATFORM_IOS_SIMULATOR 1
-#elif TARGET_OS_IPHONE
-#define HIGANCC_PLATFORM_IOS 1
-// iOS device
-#elif TARGET_OS_MAC
-// Other kinds of Mac OS
-#else
-#error "Unknown Apple platform"
-#endif
 #else
 #error "Unknown platform!"
 #endif
-
 #endif
