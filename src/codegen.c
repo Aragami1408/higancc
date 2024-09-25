29174b95543608978b4661869fdacc10d17bfb7b
Aragami1408 <vucaominh1408@gmail.com>
2024-09-25 23:31:22 +1000

    strdup and strndup are only available in posix-compliant systems

--- src/codegen.c
+++ src/codegen.c
@@ -1,5 +1,30 @@
 #include "codegen.h"
 
+#ifndef HIGANCC_PLATFORM_POSIX
+char *strdup(const char *s) {
+    usize size = strlen(s) + 1;
+    char *p = malloc(size);
+    if (p != NULL) {
+        memcpy(p, s, size);
+    }
+    return p;
+}
+
+char *strndup(const char *s, usize n) {
+    char *p;
+    usize n1;
+
+    for (n1 = 0; n1 < n && s[n1] != '\0'; n1++)
+        continue;
+    p = malloc(n + 1);
+    if (p != NULL) {
+        memcpy(p, s, n1);
+        p[n1] = '\0';
+    }
+    return p;
+}
+#endif
+
 ASMNode *convert_ast_to_asm(ArenaAllocator *a, const AST *ptr) {
   if (ptr == NULL) return NULL;
   AST ast = *ptr; 
