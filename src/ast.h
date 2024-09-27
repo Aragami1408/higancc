2a8627fab503592d8c082be375a136f3febf0669
Aragami1408 <vucaominh1408@gmail.com>
2024-09-27 23:49:13 +1000

    Proposed AST redesign

--- src/ast.h
+++ src/ast.h
@@ -5,53 +5,72 @@
 #include "memory.h"
 
 typedef enum {
-  AST_PROGRAM,
-  AST_FUNCTION,
-  AST_RETURN,
-  AST_CONSTANT,
-  AST_UNARY,
+	AST_PROGRAM,
+	AST_FUNCTION,
+	AST_RETURN,
+	AST_CONSTANT,
+	AST_UNARY,
 } ASTNodeType;
 
 typedef enum {
-  AST_UNARY_COMPLEMENT,
-  AST_UNARY_NEGATE
+	AST_UNARY_COMPLEMENT,
+	AST_UNARY_NEGATE
 } ASTOperator;
 
 typedef struct ASTList ASTList;
 typedef struct AST AST;
 
 struct ASTList {
-  AST *node;
-  ASTList *next;
+	AST *node;
+	ASTList *next;
 };
 
+/*
+struct Program:
+	ArrayList<Function> functions;
+
+struct Function:
+	Token name;
+	ArrayList<Statement> statements;
+
+struct Statement:
+	Return(Expression expr)
+
+enum ExpressionType:
+	Constant,
+	Unary
+
+struct Expression(ExpressionType):
+	Constant(int)
+	Unary(Operators unary_op, Expression expr)
+*/
 struct AST {
 
-  ASTNodeType tag;
+	ASTNodeType tag;
 
-  union {
-    struct AST_PROGRAM {
-      ASTList *functions;
-    } program;
+	union {
+		struct AST_PROGRAM {
+			ASTList *functions;
+		} program;
 
-    struct AST_FUNCTION {
-      Token name;
-      AST *body;
-    } function;
+		struct AST_FUNCTION {
+			Token name;
+			AST *body;
+		} function;
 
-    struct AST_RETURN {
-      AST *exp;  
-    } ret;
+		struct AST_RETURN {
+			AST *exp;
+		} ret;
 
-    struct AST_UNARY {
-      ASTOperator op;
-      AST *exp;
-    } unary;
+		struct AST_UNARY {
+			ASTOperator op;
+			AST *exp;
+		} unary;
 
-    struct AST_CONSTANT {
-      int data;
-    } constant;
-  } data;
+		struct AST_CONSTANT {
+			int data;
+		} constant;
+	} data;
 };
 
 AST *AST_createNode(ArenaAllocator *a, ASTNodeType type);
