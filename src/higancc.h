e9c0160393453e2dc8f43f9302bfb8c3502c948d
Cao Minh Vu <vucaominh1408@gmail.com>
2025-07-21 23:00:31 +0700

    Add tokenizer

--- src/higancc.h
+++ src/higancc.h
@@ -1,6 +1,37 @@
 #ifndef HIGANCC_H
 #define HIGANCC_H
 
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef enum {
+	TK_RESERVED,
+	TK_NUM,
+	TK_EOF
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+	TokenKind kind;
+	Token *next;
+	int val; // is set when TK_NUM
+	char *str;
+};
+
+Token *token;
+
+void error(char *fmt, ...);
+
+bool consume(char op);
+void expect(char op);
+int expect_number();
+bool at_eof();
+Token *new_token(TokenKind kind, Token *cur, char *str);
+Token *tokenize(char *p);
 
 #endif
